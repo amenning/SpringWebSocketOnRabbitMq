971adf5836f8bfeb7629ec86e77c14884defe25f
Andrew Menning <menning23@gmail.com>
2018-06-25 21:38:38 -0400

    Add websocket example

---
+++ src/main/java/com/amenning/web/MessagingController.java
@@ -0,0 +1,14 @@
+package com.amenning.web;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessagingController {
+    @MessageMapping("/message")
+    @SendTo("/topic/mural")
+    public String send(String message) throws Exception {
+        return message;
+    }
+}
